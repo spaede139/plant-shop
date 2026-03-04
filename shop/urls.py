90b580abafe50e40992159c35e5777a5c5f971d0
spaede <robi.new123456@gmail.com>
2026-03-04 10:28:17 +0300

    18lab not al

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,10 @@ urlpatterns=[
     path('',views.home,name='home'),
     path('about/',views.about_shop,name='about'),
     path('author/',views.about_author,name='author'),
+    path('catalog/' views.cotolog, name='catalog'),
+    path('catalog/<int:pk>/' views.product_detail, name='product_detail'),
+    path('cart/add/<int:product_id>/' views.add_to_cart, name='add_to_cart'),
+    path('cart/update/<int:item_id>/' views.update_cart_item, name='update_cart_item'),
+    path('cart/remove/<int:item_id>/', views.remove_from_cart name='remove_from_cart'),
+    path('cart/', views.cart_detail name='cart_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from .models import Product, Category, Manufacturer, Cart, CartItem
 
 def home(request):
     """Главная страница"""
@@ -23,3 +26,73 @@ def about_author(request):
     <p>Лабараторную сделал Рубцов Петр Группа 89 ТП</p>
     
     """)
+
+
+def product_list(request):
+    products = Product.objects.all()
+    
+
+    category = request.GET.get('category')
+    manufacturer = request.GET.get('manufacturer')
+    search = request.GET.get('search')
+    
+    if category:
+        products = products.filter(category_id=category)
+    if manufacturer:
+        products = products.filter(manufacturer_id=manufacturer)
+    if search:
+        products = products.filter(Q(nameicontains=search) | Q(descriptionicontains=search))
+    
+    return render(request, 'shop/product_list.html', {
+        'products': products,
+        'categories': Category.objects.all(),
+        'manufacturers': Manufacturer.objects.all(),
+    })
+
+def product_detail(request, pk):
+    return render(request, 'shop/product_detail.html', {
+        'product': get_object_or_404(Product, id=pk)
+    })
+
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart, _ = Cart.objects.get_or_create(user=request.user)
+    
+    item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    
+    if not created and item.quantity < product.stock:
+        item.quantity += 1
+        item.save()
+    
+    return redirect('cart_detail')
+
+
+@login_required
+def update_cart_item(request, item_id):
+    item = get_object_or_404(CartItem, id=item_id, cart__user=request.user)
+    
+    if request.method == 'POST':
+        qty = int(request.POST.get('quantity', 1))
+        if qty > 0 and qty <= item.product.stock:
+            item.quantity = qty
+            item.save()
+        elif qty <= 0:
+            item.delete()
+    
+    return redirect('cart_detail')
+
+
+@login_required
+def remove_from_cart(request, item_id):
+    get_object_or_404(CartItem, id=item_id, cart__user=request.user).delete()
+    return redirect('cart_detail')
+
+
+@login_required
+def cart_detail(request):
+    cart, _ = Cart.objects.get_or_create(user=request.user)
+    return render(request, 'shop/cart_detail.html', {
+        'cart': cart,
+        'items': cart.cartitem_set.all()
+    })
